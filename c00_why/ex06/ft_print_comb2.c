fe5d753aa2a258c91ea2590d41b4fa8b77516c42
Dahyun Chung <dachung@c3r6s6.42seoul.kr>
2020-02-01 12:53:14 +0900

    1253

---
+++ c00_why/ex06/ft_print_comb2.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dachung <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/25 22:26:44 by dachung           #+#    #+#             */
+/*   Updated: 2020/01/26 18:36:09 by dachung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	while (i < 100)
+	{
+		while (j < 100)
+		{
+			ft_putchar((i / 10) + '0');
+			ft_putchar((i % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((j / 10) + '0');
+			ft_putchar((j % 10) + '0');
+			if (i + j != 197)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+}
+
+int		main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
